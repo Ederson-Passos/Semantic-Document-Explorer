dd9a76b8d5ec433d8e620f6f220b7cb763bfdd2d
Ederson-Passos <ederson.minimus@gmail.com>
2025-04-10 11:21:17 -0300

    Verifica se a uma GPU disponível e a utiliza.

--- EmbeddingGenerator.py
+++ EmbeddingGenerator.py
@@ -1,12 +1,12 @@
 import os
 import numpy as np
 from transformers import BertTokenizer, TFBertModel
+import tensorflow as tf
 
 class EmbeddingGenerator:
     def __init__(self, model_name='bert-base-uncased', batch_size=32, output_dir='embeddings_tf'):
         """
-        Inicializa o gerador de embeddings com o modelo TensorFlow, o tamanho do lote para processamento e
-        o diretório para salvar os embeddings.
+        Inicializa o gerador de embeddings com o modelo TensorFlow, gerando embeddings a partir de tokens.
         Args:
             model_name (str): O nome do modelo Transformer pré-treinado a ser usado.
             batch_size (int): O número de sequências a serem processadas por lote.
@@ -18,9 +18,17 @@ class EmbeddingGenerator:
         self.output_dir = output_dir
         os.makedirs(self.output_dir, exist_ok=True)
 
+        # Verifica se a GPU está disponível e a usa, caso contrário usa a CPU
+        if tf.config.list_physical_devices('GPU'):
+            print("GPU encontrada, usando para geração de embeddings.")
+            self.device = '/GPU:0'
+        else:
+            print("Nenhuma GPU encontrada, usando CPU para geração de embeddings.")
+            self.device = '/CPU:0'
+
     def generate_embeddings(self, tokens_list, filename_prefix="document"):
         """
-        Gera embeddings para uma lista de tokens, processando-os em lotes usando TensorFlow.
+        Gera embeddings para uma lista de tokens, processando-os em lotes usando TensorFlow e a device configurada.
         Args:
             tokens_list (list): Uma lista de tokens.
             filename_prefix (str): Prefixo para o nome do arquivo onde os embeddings serão salvos.
@@ -41,11 +49,12 @@ class EmbeddingGenerator:
                                     truncation=True,
                                     max_length=512)
 
-            outputs = self.model(**inputs, output_hidden_states=True)
-
-            # O último hidden state é uma sequência de vetores para cada token.
-            # Pega a representação do token [CLS] como a representação da sequência.
-            embeddings = outputs.hidden_states[-1][:, 0, :].numpy() # (batch_size, embedding_dimension)
+            # Envolve a inferência do modelo no contexto do dispositivo.
+            with tf.device(self.device):
+                outputs = self.model(**inputs, output_hidden_states=True)
+                # O último hidden state é uma sequência de vetores para cada token.
+                # Pega a representação do token [CLS] como a representação da sequência.
+                embeddings = outputs.hidden_states[-1][:, 0, :].numpy() # (batch_size, embedding_dimension)
             all_embeddings.extend(embeddings)
 
         output_filename = os.path.join(self.output_dir, f"{filename_prefix}_embeddings.npy")
